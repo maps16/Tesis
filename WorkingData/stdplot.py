3c0d1bee7208fc3110d1ed67bba725592ca867a0
maps16 <mapsjr16@gmail.com>
2022-07-13 13:36:30 -0700

    Plot Desviacion estandard de todo

---
+++ WorkingData/stdplot.py
@@ -0,0 +1,65 @@
+import numpy as np
+from glob import glob
+from scipy.stats import norm as scp
+import matplotlib.pyplot as plt
+import h5py as h5
+
+# Loc de archivos para trabajar
+data = "subhalo"
+run = glob('WorkingData/StandardResolution/*') #Ubicanco las carpetas de las diferentes cosmologias
+run.sort()
+
+# Corriendo sobre las diferentes cosmologias
+for x in run:
+    x = x.split('/')[-1]
+    namepath = "/home/martin/Documentos/Tesis/WorkingData/StandardResolution/" + x + "/" + data
+   
+    #Checar todos los archivos
+    arch = glob(namepath + '/*')
+    arch.sort()
+
+    # Extableciendo/Limpiendo los arrays para diferentes valores de interes
+    mean = []       # Media
+    std  = []       # Desviacion Estandar
+    z = []       # Redshift
+
+    # Iterar sobre todos los archivos
+    for i in arch:
+
+        # Accediendo al archivo
+        file_data = h5.File(i, 'r')
+
+        # Trabajando solo con los archivos que contienen subhalos
+        if 'Subhalo' and 'Group' in file_data:
+            
+            # Extrayendo parametros de la simulación
+            Omega0, OmegaL, OmegaB, z_cal = file_data['Parameters'].attrs['Omega0'], file_data['Parameters'].attrs['OmegaLambda'], file_data['Parameters'].attrs['OmegaBaryon'], file_data[ 'Header' ].attrs[ 'Redshift' ]
+            
+            # Label identificando cada cosmologia
+            nameParam = r'$\Omega_0=$'+str(Omega0) + ', ' + r'$\Omega_\lambda=$'+str(OmegaL) + ', ' + r'$\Omega_B=$'+str(OmegaB) 
+            
+            # Extrayendo la masa y calculando su Log10
+            logmass = np.log10( file_data['Subhalo']['SubhaloMass'][:] * 1e10)
+
+            # Calculado los parametros para el ajuste
+            loc, scale = scp.fit(logmass,)
+            # Calculo de la media y desviacion estandar
+            mean_cal, std_cal = scp.mean(loc,scale), scp.std(loc,scale)
+
+            # Guardando los valoares en los arrays
+            mean.append(mean_cal)
+            std.append(std_cal)
+            z .append(z_cal)
+
+        file_data.close()
+
+    if len(std) != 0 : plt.plot(z, std, label=nameParam)
+        
+
+plt.xlabel('Redshift')
+plt.ylabel('$\sigma$')
+plt.xlim((15.5,-0.5))
+plt.legend(loc='best')
+plt.show()
+
+
