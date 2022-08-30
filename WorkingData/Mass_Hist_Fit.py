334b1d7028ac6205a4447d4e9c724673dfbc01a5
maps16 <mapsjr16@gmail.com>
2022-08-30 08:45:45 -0700

    Actualizando los graficos

--- WorkingData/Mass_Hist_Fit.py
+++ WorkingData/Mass_Hist_Fit.py
@@ -7,9 +7,11 @@ from scipy.stats import exponnorm as scp
 
 # Creando Figura para plot
 #NUM_COLORS = 5
-fig ,ax = plt.subplots( nrows=3, ncols=3, figsize=(16,10) )
-#cm =  plt.get_cmap('gist_gray')
-#ax[-1,-1].set_prop_cycle('color', [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)] )
+fig ,ax = plt.subplots( nrows=4, ncols=5, figsize=(16,10), num='MassDistCanonRunSep' )
+fig2 ,ax2 = plt.subplots(nrows=1, ncols=1 ,num='MassDistCanonRun', figsize=(5.5,5.5) )
+NUM_COLORS = 20#len(arch)
+cm =  plt.get_cmap('tab20')
+ax2.set_prop_cycle('color', [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)] )
 
 def plotAx(pos, pdata, nameData, num_bin, *param):
     '''
@@ -31,7 +33,7 @@ def plotAx(pos, pdata, nameData, num_bin, *param):
     for string in simple:
         simplename += string + ' '
     
-
+    plt.figure('MassDistCanonRunSep')
     # Plotting Hist
     ax.flat[pos].hist(pdata , bins=num_bin, range=(np.min(pdata),np.max(pdata)), density = False)
     
@@ -39,7 +41,9 @@ def plotAx(pos, pdata, nameData, num_bin, *param):
     ax.flat[pos].plot( X, scp.pdf(X, k,loc,scale) * len(pdata) * bsz, label= nameData)
 
     #Plot Acumulado
-    ax[-1,-1].hist(pdata , bins=num_bin, range=(np.min(pdata),np.max(pdata)), density = False, label=simplename, alpha=0.5)
+    plt.figure('MassDistCanonRun')
+    #ax2.hist(pdata , bins=num_bin, range=(np.min(pdata),np.max(pdata)), density = False, label=simplename, alpha=0.9)
+    ax2.plot( X, scp.pdf(X, k, loc, scale) * len(pdata) * bsz, label=simplename)
     
     # Ajustes de la figura
     #ax.flat[pos].set_xlim(round(np.min(pdata))  , 15.)
@@ -54,55 +58,68 @@ def plotAx(pos, pdata, nameData, num_bin, *param):
 data_Name = 'subhalo'                                                   # Tipo de Dato
 path = '/home/martin/Documentos/Tesis/WorkingData/StandardResolution'   # Ubicacion
 # Identtificando el snapshot 017 del catalogo de halos
-archivos = glob( path + '/*/' + data_Name + '/*017.hdf5')
+archivos = glob( path + '/RunCanonica/' + data_Name + '/*.hdf5')
 archivos.sort()
 
 temp_exit = 0
 bins = 55
 for i in archivos:
-
-    #Abriendo el archivo
+    
+        #Abriendo el archivo
     file_data = h5.File(i, mode='r')
-    Omega0, OmegaL, OmegaB = file_data['Parameters'].attrs['Omega0'], file_data['Parameters'].attrs['OmegaLambda'], file_data['Parameters'].attrs['OmegaBaryon'] # Obtenido paramtros cosmologicos
+    if 'Subhalo' and 'Group' in file_data:
+
+        Omega0, OmegaL, OmegaB, redshift = file_data['Parameters'].attrs['Omega0'], file_data['Parameters'].attrs['OmegaLambda'], file_data['Parameters'].attrs['OmegaBaryon'], file_data['Header'].attrs['Redshift'] # Obtenido paramtros cosmologicos
+        
+        # Extrayendo las masas de los halos
+        mass = file_data['Subhalo']['SubhaloMass'][:] * 1e10
+        logmass = np.log10(mass)
+        
+        # Calculo de los paramtros Exponencial Normal
+        k, loc, scale = scp.fit(logmass)
+
+        # LABEL, escrito de los labels
+        mean, std = scp.mean(k,loc,scale), scp.std(k,loc,scale)             #Calculo de Mean y STD
+        nameParam = r'$\Omega_0=$'+str(Omega0) + ', ' + r'$\Omega_\lambda=$'+str(OmegaL) + ', ' + r'$\Omega_B=$'+str(OmegaB) + '\n  Mean =' + str(round(mean, ndigits=4)) + ', std =' + str(round(std,ndigits=4))
+        nameParam = 'z = ' +str( round(redshift,1) )
     
-    # Extrayendo las masas de los halos
-    mass = file_data['Subhalo']['SubhaloMass'][:] * 1e10
-    logmass = np.log10(mass)
-    
-    # Calculo de los paramtros Exponencial Normal
-    k, loc, scale = scp.fit(logmass)
 
-    # LABEL, escrito de los labels
-    mean, std = scp.mean(k,loc,scale), scp.std(k,loc,scale)             #Calculo de Mean y STD
-    nameParam = r'$\Omega_0=$'+str(Omega0) + ', ' + r'$\Omega_\lambda=$'+str(OmegaL) + ', ' + r'$\Omega_B=$'+str(OmegaB) + '\n  Mean =' + str(round(mean, ndigits=4)) + ', std =' + str(round(std,ndigits=4))
+        # Funcion de Ploteo Checar Funciones 
+        plotAx(temp_exit, logmass, nameParam, bins, k,loc,scale, mean, std  )
 
-   
+        #Calculo de Media, STD en pantalla
+        # print('Mean: ' +  str( mean ) )
+        # print('STD : ' +  str( std ) )
 
-    # Funcion de Ploteo Checar Funciones 
-    plotAx(temp_exit, logmass, nameParam, bins, k,loc,scale, mean, std  )
-
-    #Calculo de Media, STD en pantalla
-    # print('Mean: ' +  str( mean ) )
-    # print('STD : ' +  str( std ) )
-
-    # Debug
-    # print( logmass )
-    # print(Omega0, OmegaL, OmegaB , Omega0+OmegaB+OmegaL)
-    # if temp_exit == 0:
-    #     plt.legend(loc=1)
-    #     plt.show() # Uncomment for only one run
-    #     exit()
-    temp_exit += 1
+        # Debug
+        # print( logmass )
+        # print(Omega0, OmegaL, OmegaB , Omega0+OmegaB+OmegaL)
+        # if temp_exit == 0:
+        #     plt.legend(loc=1)
+        #     plt.show() # Uncomment for only one run
+        #     exit()
+        temp_exit += 1
     file_data.close()
 
 # Ajuste de la figura
-ax.flat[-1].legend(loc=1)
-ax.flat[-1].set_xlim(9.9,15.)
-ax.flat[-1].set_ylabel("N bin")
-ax.flat[-1].set_xlabel('log$_{10}$ M$_\odot$')
+plt.figure('MassDistCanonRunSep')
 #plt.tight_layout(h_pad = hspace, w_pad=wspace ,rect=(left,bottom,right,top))
-plt.tight_layout(h_pad=0.108,w_pad=0.116,rect=(0.0,0.0,1.0,1.0))
-plt.show()
+plt.tight_layout(h_pad=0.001,w_pad=0.001,rect=(0.0,0.0,1.0,1.0))
+# plt.tight_layout()
+plt.savefig('Documento/images/MassDistCanonRunSep.png')
+
+
+plt.figure('MassDistCanonRun')
+ax2.legend(loc='best')
+ax2.set_xlim(9.9,15.)
+ax2.set_ylim(-25,3050)
+ax2.set_ylabel("N bin")
+ax2.set_xlabel('log$_{10}$ M$_\odot$')
+plt.tight_layout()
+plt.savefig('Documento/images/MassDistCanonRun.png')
+
+plt.close('all')
+# plt.show()
 '''
 top=0.988,
 bottom=0.048,
